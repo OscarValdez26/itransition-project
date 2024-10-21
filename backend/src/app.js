ad06ebcee02d4bea8a5891285c2a9e04799d9d7f
OscarValdez26 <oscarrvaldez25@gmail.com>
2024-10-21 00:36:23 -0600

    updated cors

--- backend/src/app.js
+++ backend/src/app.js
@@ -9,7 +9,7 @@ const app = express();
 
 app.disable('x-powered-by');
 app.use(morgan('dev'));
-app.use(cors({origin:'*',credentials:true}));
+app.use(cors({origin:process.env.FRONTEND_URL,credentials:true}));
 app.use(express.json());
 app.use(cookieParser());
 app.use(router);
